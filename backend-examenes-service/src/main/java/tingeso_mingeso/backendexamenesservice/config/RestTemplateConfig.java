39296df60e198bae49d1ffa7058b46663c30b13d
Argenis <argenis.benitez@usach.cl>
2023-11-13 00:15:06 -0300

    [Argenis] version con postgres funcional con minikube

--- backend-examenes-service/src/main/java/tingeso_mingeso/backendexamenesservice/config/RestTemplateConfig.java
+++ backend-examenes-service/src/main/java/tingeso_mingeso/backendexamenesservice/config/RestTemplateConfig.java
@@ -1,12 +1,13 @@
 package tingeso_mingeso.backendexamenesservice.config;
 
+import org.springframework.cloud.client.loadbalancer.LoadBalanced;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.client.RestTemplate;
 
 @Configuration
 public class RestTemplateConfig {
-
+    @LoadBalanced
     @Bean
     public RestTemplate restTemplate() {
         return new RestTemplate();
